a5414002c2f2a3dad3cab8194b2f88e2a4caa995
Jenny Sahng <6475953+jennynz@users.noreply.github.com>
2024-08-09 13:53:19 +1200

    feat: get it returning something on live server

--- README.md
+++ README.md
@@ -3,18 +3,44 @@
 Estimates a household's emissions and cost savings from electrification
 
 
+## Installation
+
 ```bash
-# Install
 pipenv install --dev
+```
 
+## Run
+
+Run the live server with the following command:
+
+```bash
 cd src
-
-# Run the live server
 pipenv run fastapi dev main.py
+```
 
+You can check out the auto-generated API docs at http://127.0.0.1:8000/docs.
+
+
+## Contributing
+
+```bash
 # Run tests
 pipenv run python -m pytest
 
 # Lint (skip string normalisation)
 pipenv run black -S .
 ```
+
+## Refreshing `src/client` from `openapi.yml`
+
+Since FastAPI expected pydantic models, we need to use an OpenAPI python generator that specifically produces pydantic models. We're using [python-pydantic-v1](https://github.com/OpenAPITools/openapi-generator/blob/master/docs/generators/python-pydantic-v1.md).
+
+```bash
+brew install openapi-generator
+
+# navigate to the root dir of this repo (make sure you're not in src/)
+
+openapi-generator generate -i openapi.yml -g python-pydantic-v1 --additional-properties=generateSourceCodeOnly=true
+mv openapi_client src/
+```
+
--- src/main.py
+++ src/main.py
@@ -1,99 +1,6 @@
 from fastapi import FastAPI
-from pydantic import BaseModel, ConfigDict
-from pydantic.types import conint
-from typing import List
-from enum import Enum
-from client.models import Household, Savings
+from openapi_client.models import Household, Savings
 app = FastAPI()
-
-ConfigDict.arbitrary_types_allowed = True  
-
-
-# # TODO: turn these into actual form inputs
-# class LocationEnum(str, Enum):
-#     NORTHLAND = "Northland / Te Tai Tokerau"
-#     GREAT_BARRIER_ISLAND = "Great Barrier Island / Aotea"
-#     AUCKLAND = "Auckland / Tāmaki Makaurau"
-#     WAIKATO = "Waikato"
-#     BAY_OF_PLENTY = "Bay of Plenty / Te Moana-a-Toi"
-#     GISBORNE = "Gisborne / Tairāwhiti"
-#     HAWKES_BAY = "Hawke's Bay / Te Matau-a-Māui"
-#     TARANAKI = "Taranaki"
-#     MANAWATU_WANGANUI = "Manawatu-Wanganui"
-#     WELLINGTON = "Wellington / Te Whanganui a Tara"
-#     TASMAN = "Tasman / Te Tai o Aorere"
-#     NELSON = "Nelson / Whakatū"
-#     MARLBOROUGH = "Marlborough / Te Tauihu-o-te-waka"
-#     WEST_COAST = "West Coast / Te Tai Poutini"
-#     CANTERBURY = "Canterbury / Waitaha"
-#     CHRISTCHURCH = "Christchurch / Ōtautahi"
-#     OTAGO = "Otago"
-#     DUNEDIN = "Dunedin / Ōtepoti"
-#     SOUTHLAND = "Southland / Murihiku"
-#     STEWART_ISLAND = "Stewart Island / Rakiura"
-#     CHATHAM_ISLANDS = "Chatham Islands / Rēkohu / Wharekauri"
-#     OVERSEAS = "Overseas"
-#     OTHER = "Other"
-
-
-# class SpaceHeaterEnum(str, Enum):
-#     ELECTRIC_HEAT_PUMP = "Electric heat pump"
-#     ELECTRIC_RESISTANCE = "Electric resistance"
-#     GAS = "Gas (piped/ducted)"
-#     LPG = "LPG bottles"
-#     WOOD = "Wood burner"
-#     DONT_KNOW = "Don't know"
-
-
-# class WaterHeaterEnum(str, Enum):
-#     ELECTRIC_HEAT_PUMP = "Electric heat pump"
-#     ELECTRIC_RESISTANCE = "Electric resistance"
-#     GAS = "Gas (piped/ducted)"
-#     LPG = "LPG bottles"
-#     SOLAR = "Solar water heater"
-#     WOOD = "Wood burner"
-#     DONT_KNOW = "Don't know"
-
-
-# class CooktopEnum(str, Enum):
-#     ELECTRIC_INDUCTION = "Electric induction"
-#     ELECTRIC_RESISTANCE = "Electric resistance"
-#     GAS = "Gas (piped/ducted)"
-#     LPG = "LPG bottles"
-#     WOOD = "Wood"
-#     DONT_KNOW = "Don't know"
-
-
-# class FuelTypeEnum(str, Enum):
-#     PETROL = "Petrol"
-#     DIESEL = "Diesel"
-#     ELECTRIC = "Electric (BEV)"
-#     PLUG_IN_HYBRID = "Plug-in Hybrid (PHEV)"
-#     HYBRID = "Hybrid (HEV)"
-
-
-# class Vehicle(BaseModel):
-#     fuel_type: FuelTypeEnum
-#     kms_per_week: int
-
-
-# class Solar(BaseModel):
-#     has_solar: bool
-#     size: int
-
-
-# class Battery(BaseModel):
-#     fuel_type: FuelTypeEnum
-#     kms_per_week: int
-
-
-# class Household(BaseModel):
-#     location: LocationEnum
-#     occupancy: conint(ge=1, le=100)
-#     space_heater: SpaceHeaterEnum
-#     water_heater: WaterHeaterEnum
-#     cooktop: CooktopEnum
-#     vehicles: List[Vehicle]
 
 
 @app.get("/")
@@ -106,8 +13,8 @@ def health_check():
     return {"status": "healthy"}
 
 
-@app.post("/savings", response_model=None) 
-def calculate_household_savings(household: Household):
+@app.post("/savings") 
+def calculate_household_savings(household: Household) -> Savings:
     savings = {
         "emissions": {
             "perWeek": {
