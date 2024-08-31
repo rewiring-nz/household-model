ab375edd3ffbcb3d8065757adce0044a84e69452
Jenny Sahng <6475953+jennynz@users.noreply.github.com>
2024-08-31 20:35:52 +1200

    feat: update emissions factors & energy prices

--- src/constants/fuel_stats.py
+++ src/constants/fuel_stats.py
@@ -11,33 +11,32 @@ class FuelTypeEnum(str, Enum):
     SOLAR = "SOLAR"  # this is direct solar, e.g. roof solar water heaters
 
 
-# From 'Misc'!B154
+# https://environment.govt.nz/assets/publications/Measuring-Emissions-Guidance_EmissionFactors_Summary_2023_ME1781.pdf
 # Unit: kgCO2e/kWh
 EMISSIONS_FACTORS = {
-    "electricity": 0.098,
-    # 'electricity': 0.0,  # assuming 100% renewable grid
-    "natural_gas": 0.217,
-    "lpg": 0.218,
-    "wood": 0.025,
-    "petrol": 0.242,
+    "electricity": 0.074,
+    "natural_gas": 0.201,
+    "lpg": 0.219,
+    "wood": 0.016,
+    "petrol": 0.258,
     "diesel": 0.253,
     "solar": 0,
-    # Excluding coal for now, as it skews results
-    # 'coal': 0.9,  # TODO: check this, UK source from https://www.rensmart.com/Calculators/KWH-to-CO2
 }
 
-# From 'Energy prices'
+# From 'Energy prices' C7:C18
 # Unit: $/kWh
+# https://docs.google.com/spreadsheets/d/1_eAAx5shTHSJAUuHdfj7AQafS0BZJn_0F48yngCpFXI/edit?gid=901618629#gid=901618629
 COST_PER_FUEL_KWH_TODAY = {
-    "electricity": 0.242,  # today's grid average flat, G14
-    "natural_gas": 0.11,  # C45
-    "lpg": 0.244,  # C51
-    "wood": 0.113,  # C54
-    "petrol": 0.27,  # C57
-    "diesel": 0.21,  # C58
+    "electricity": 0.262,
+    "natural_gas": 0.118,
+    "lpg": 0.255,
+    "wood": 0.113,
+    "petrol": 0.289,
+    "diesel": 0.197,
     "solar": 0,
 }
 # TODO: Use this price when doing lifetime opex calculations
+# TODO: Update this with average or nominal price for next 15 years from https://docs.google.com/spreadsheets/d/1_eAAx5shTHSJAUuHdfj7AQafS0BZJn_0F48yngCpFXI/edit?gid=901618629#gid=901618629
 COST_PER_FUEL_KWH_AVG_15_YEARS = {
     "electricity": 0.282,
     "natural_gas": 0.123,
