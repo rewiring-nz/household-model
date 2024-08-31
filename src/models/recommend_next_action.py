a5a5f42e3f2a3824ee885bb6265b062f2b98efd4
Jenny Sahng <6475953+jennynz@users.noreply.github.com>
2024-09-01 00:59:33 +1200

    feat: recommend random action (#12)

--- src/models/recommend_next_action.py
+++ src/models/recommend_next_action.py
@@ -1,8 +1,20 @@
+import random
 from openapi_client.models import Household, Recommendation, RecommendationActionEnum
+
+NEXT_STEP_URLS = {
+    RecommendationActionEnum.SPACE_HEATING: "https://www.rewiring.nz/electrification-guides/space-heating-and-cooling",
+    RecommendationActionEnum.WATER_HEATING: "https://www.rewiring.nz/electrification-guides/water-heating",
+    RecommendationActionEnum.COOKING: "https://www.rewiring.nz/electrification-guides/cooktops",
+    RecommendationActionEnum.VEHICLE: "https://www.rewiring.nz/electrification-guides/electric-cars",
+    RecommendationActionEnum.SOLAR: "https://www.rewiring.nz/electrification-guides/solar",
+    RecommendationActionEnum.BATTERY: "https://www.rewiring.nz/electrification-guides/home-batteries",
+}
 
 
 def recommend_next_action(household: Household) -> Recommendation:
+    # TODO: actually implement the algorithm, this is just a dummy filler
+    next_action = random.choice(list(RecommendationActionEnum))
     return Recommendation(
-        action=RecommendationActionEnum("SPACE_HEATING"),
-        url="https://www.rewiring.nz/electrification-guides/space-heating-and-cooling",
+        action=next_action,
+        url=NEXT_STEP_URLS.get(next_action),
     )
