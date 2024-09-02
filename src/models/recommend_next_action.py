6c4c4bbfdf47de80984a2f58a5a135c2fb3012b5
Jenny Sahng <6475953+jennynz@users.noreply.github.com>
2024-09-02 18:03:15 +1200

    feat: recommend next action

--- src/models/recommend_next_action.py
+++ src/models/recommend_next_action.py
@@ -1,5 +1,14 @@
-import random
+from typing import List
+from models.electrify_household import (
+    electrify_cooktop,
+    electrify_space_heating,
+    electrify_vehicle,
+    electrify_water_heating,
+    should_electrify,
+    should_install,
+)
 from openapi_client.models import Household, Recommendation, RecommendationActionEnum
+from openapi_client.models.vehicle import Vehicle
 
 NEXT_STEP_URLS = {
     RecommendationActionEnum.SPACE_HEATING: "https://www.rewiring.nz/electrification-guides/space-heating-and-cooling",
@@ -8,13 +17,61 @@ NEXT_STEP_URLS = {
     RecommendationActionEnum.VEHICLE: "https://www.rewiring.nz/electrification-guides/electric-cars",
     RecommendationActionEnum.SOLAR: "https://www.rewiring.nz/electrification-guides/solar",
     RecommendationActionEnum.BATTERY: "https://www.rewiring.nz/electrification-guides/home-batteries",
+    RecommendationActionEnum.FULLY_ELECTRIFIED: None,
 }
 
 
 def recommend_next_action(household: Household) -> Recommendation:
-    # TODO: actually implement the algorithm, this is just a dummy filler
-    next_action = random.choice(list(RecommendationActionEnum))
-    return Recommendation(
-        action=next_action,
-        url=NEXT_STEP_URLS.get(next_action),
-    )
+    """Heuristic algorithm to recommend the next step action based on household inputs
+
+    Args:
+        household (Household): household inputs
+
+    Returns:
+        Recommendation: the next step that the user should take for electrification
+    """
+    action = RecommendationActionEnum.FULLY_ELECTRIFIED
+
+    # In priority order:
+
+    # 1. Rooftop solar
+    if should_install(household.solar):
+        action = RecommendationActionEnum.SOLAR
+
+    # 2. First EV
+    if n_vehicles_to_electrify(household.vehicles) > 0:
+        action = RecommendationActionEnum.VEHICLE
+
+    # 3. Space heater
+    if should_electrify(household.space_heating, electrify_space_heating):
+        action = RecommendationActionEnum.SPACE_HEATING
+
+    # 4. Water heater
+    if should_electrify(household.water_heating, electrify_water_heating):
+        action = RecommendationActionEnum.WATER_HEATING
+
+    # 5. Cooktop
+    if should_electrify(household.cooktop, electrify_cooktop):
+        action = RecommendationActionEnum.COOKING
+
+    # 6. Battery
+    if should_install(household.battery):
+        action = RecommendationActionEnum.BATTERY
+
+    # 7. All other vehicles
+    if n_vehicles_to_electrify(household.vehicles) > 1:
+        action = RecommendationActionEnum.SPACE_HEATING
+
+    return Recommendation(action=action, url=NEXT_STEP_URLS.get(action))
+
+
+def n_vehicles_to_electrify(vehicles: List[Vehicle]) -> int:
+    """Calculates the number of vehicles in the list to electrify
+
+    Args:
+        vehicles (List[Vehicle]): list of vehicles and their fuel type info
+
+    Returns:
+        int: the number of vehicles still to electrify
+    """
+    return sum([should_electrify(v, electrify_vehicle) for v in vehicles])
