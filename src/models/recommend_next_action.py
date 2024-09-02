1e362312348c79e6fd2773f3371f24245478c5b0
Jenny Sahng <6475953+jennynz@users.noreply.github.com>
2024-09-02 18:33:19 +1200

    fix: recommend next action (#19)
    * feat: recommend next action
    
    * fix: recommendation algorithm

--- src/models/recommend_next_action.py
+++ src/models/recommend_next_action.py
@@ -9,6 +9,8 @@ from models.electrify_household import (
 )
 from openapi_client.models import Household, Recommendation, RecommendationActionEnum
 from openapi_client.models.vehicle import Vehicle
+from openapi_client.models.vehicle_fuel_type_enum import VehicleFuelTypeEnum
+
 
 NEXT_STEP_URLS = {
     RecommendationActionEnum.SPACE_HEATING: "https://www.rewiring.nz/electrification-guides/space-heating-and-cooling",
@@ -39,32 +41,47 @@ def recommend_next_action(household: Household) -> Recommendation:
         action = RecommendationActionEnum.SOLAR
 
     # 2. First EV
-    if n_vehicles_to_electrify(household.vehicles) > 0:
+    elif (
+        n_vehicles_to_electrify(household.vehicles) > 0
+        and n_evs(household.vehicles) == 0
+    ):
         action = RecommendationActionEnum.VEHICLE
 
     # 3. Space heater
-    if should_electrify(household.space_heating, electrify_space_heating):
+    elif should_electrify(household.space_heating, electrify_space_heating):
         action = RecommendationActionEnum.SPACE_HEATING
 
     # 4. Water heater
-    if should_electrify(household.water_heating, electrify_water_heating):
+    elif should_electrify(household.water_heating, electrify_water_heating):
         action = RecommendationActionEnum.WATER_HEATING
 
     # 5. Cooktop
-    if should_electrify(household.cooktop, electrify_cooktop):
+    elif should_electrify(household.cooktop, electrify_cooktop):
         action = RecommendationActionEnum.COOKING
 
     # 6. Battery
-    if should_install(household.battery):
+    elif should_install(household.battery):
         action = RecommendationActionEnum.BATTERY
 
     # 7. All other vehicles
-    if n_vehicles_to_electrify(household.vehicles) > 1:
-        action = RecommendationActionEnum.SPACE_HEATING
+    elif n_vehicles_to_electrify(household.vehicles) > 0:
+        action = RecommendationActionEnum.VEHICLE
 
     return Recommendation(action=action, url=NEXT_STEP_URLS.get(action))
 
 
+def n_evs(vehicles: List[Vehicle]) -> int:
+    """Calculates the number of EVs vehicles in the list
+
+    Args:
+        vehicles (List[Vehicle]): list of vehicles and their fuel type info
+
+    Returns:
+        int: the number of EVs in the list
+    """
+    return sum([v.fuel_type == VehicleFuelTypeEnum.ELECTRIC for v in vehicles])
+
+  
 def n_vehicles_to_electrify(vehicles: List[Vehicle]) -> int:
     """Calculates the number of vehicles in the list to electrify
 
