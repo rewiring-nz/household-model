3cea1182de3b5c59c297573428571d916b375f8c
Jenny Sahng <6475953+jennynz@users.noreply.github.com>
2024-11-05 14:03:17 +1300

    feat: update emissions factors & energy prices (#6)
    * feat: update emissions factors & energy prices
    
    * feat: update energy prices

--- src/constants/fuel_stats.py
+++ src/constants/fuel_stats.py
@@ -11,41 +11,42 @@ class FuelTypeEnum(str, Enum):
     SOLAR = "solar"  # this is direct solar, e.g. roof solar water heaters
 
 
-# TODO: key the following dicts on FuelTypeEnum as the key, rather than its value
 
+# TODO: key the following dicts on FuelTypeEnum as the key, rather than its value
 # From 'Misc'!B154
+# Original source: https://environment.govt.nz/assets/publications/Measuring-Emissions-Guidance_EmissionFactors_Summary_2023_ME1781.pdf
 # Unit: kgCO2e/kWh
 EMISSIONS_FACTORS = {
-    "electricity": 0.098,
-    # 'electricity': 0.0,  # assuming 100% renewable grid
-    "natural_gas": 0.217,
-    "lpg": 0.218,
-    "wood": 0.025,
-    "petrol": 0.242,
+    "electricity": 0.074,
+    "natural_gas": 0.201,
+    "lpg": 0.219,
+    "wood": 0.016,
+    "petrol": 0.258,
     "diesel": 0.253,
     "solar": 0,
-    # Excluding coal for now, as it skews results
-    # 'coal': 0.9,  # TODO: check this, UK source from https://www.rensmart.com/Calculators/KWH-to-CO2
 }
 
-# From 'Energy prices'
+# 2024 prices from 'Energy prices' C7:C18
 # Unit: $/kWh
 COST_PER_FUEL_KWH_TODAY = {
-    "electricity": 0.242,  # today's grid average flat, G14
-    "natural_gas": 0.11,  # C45
-    "lpg": 0.244,  # C51
-    "wood": 0.113,  # C54
-    "petrol": 0.27,  # C57
-    "diesel": 0.21,  # C58
+    "electricity": 0.26175,
+    "natural_gas": 0.118,
+    "lpg": 0.25452,
+    "wood": 0.11250,
+    "petrol": 0.28884,
+    "diesel": 0.19679,
     "solar": 0,
 }
+
+# Average over next 15 years (real)
 # TODO: Use this price when doing lifetime opex calculations
+# Unit: $/kWh
 COST_PER_FUEL_KWH_AVG_15_YEARS = {
-    "electricity": 0.282,
-    "natural_gas": 0.123,
-    "lpg": 0.273,
-    "wood": 0.112,
-    "petrol": 0.273,
-    "diesel": 0.206,
+    "electricity": 0.29515,
+    "natural_gas": 0.14161,
+    "lpg": 0.30544,
+    "wood": 0.12837,
+    "petrol": 0.36584,
+    "diesel": 0.24925,
     "solar": 0,
 }
